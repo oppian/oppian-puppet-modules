16fb4c8a5325ce4219d59a5e75267f3767f71e93
Matthew Jacobi <matt@oppian.com>
2012-05-22 19:37:01 +0100

    Varnish: added logging, media url, require/befores remove cookies for media url

--- modules/varnish/manifests/init.pp
+++ modules/varnish/manifests/init.pp
@@ -2,18 +2,20 @@ class varnish(
     $port=80, 
     $size="1G",
     $backend_port=8080,
-    $backend_server="127.0.0.1"
+    $backend_server="127.0.0.1",
+    $media_url="^/m/"
 ) {
 
   package { 'varnish':
     ensure => present,
-    before => [File['/etc/init.d/varnish'],File['/etc/varnish/default.vcl']],
   }
 
   file { '/etc/init.d/varnish' :
     ensure => file,
     mode => 700,
     content => template('varnish/varnish.init.erb'),
+    require => Package['varnish'],
+    before => Service["varnish"],
   }
 
   file { '/etc/varnish/default.vcl' :
@@ -22,8 +24,9 @@ class varnish(
     owner => varnish,
     group => varnish,
     content => template('varnish/default.vcl.erb'),
+    require => Package['varnish'],
+    before => Service["varnish"],
   }
-
 
   service { 'varnish' :
     ensure => running,
@@ -32,5 +35,18 @@ class varnish(
     hasstatus => true,
     require => Package['varnish'],
     subscribe => [File['/etc/init.d/varnish'],File['/etc/varnish/default.vcl']],
+  }
+
+  service { 'varnishncsa' :
+  	ensure => running,
+    enable => true,
+    hasrestart => true,
+    hasstatus => true,
+    require => Service['varnish'],
+  }  
+  
+  monit::monitor { "varnish":
+    pidfile => "/var/run/varnish.pid",
+    require => Service["varnish"],
   }
 }
